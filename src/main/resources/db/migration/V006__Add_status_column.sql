00bff0bfc1624d11fb89db89f27621f18cb2c1bc
Leszek Gonczar <gonczar@gmail.com>
2018-03-29 16:24:51 +0200

    Modify migration script to initialise status column values (#47)
    The script hasn't been applied yet, which allows it to be changed.

--- src/main/resources/db/migration/V006__Add_status_column.sql
+++ src/main/resources/db/migration/V006__Add_status_column.sql
@@ -1,2 +1,17 @@
 ALTER TABLE letters
 ADD COLUMN status VARCHAR(256);
+
+UPDATE
+  letters
+SET
+  status = (
+    CASE
+      WHEN printed_at IS NULL THEN
+        'Uploaded'
+      ELSE
+        'Posted'
+    END
+  );
+
+ALTER TABLE letters
+ALTER COLUMN status SET NOT NULL;
