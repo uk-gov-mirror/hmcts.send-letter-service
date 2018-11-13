b8f8d62cbfd4c2bc16840f276eb7750994043a69
donatas M <doncem@users.noreply.github.com>
2018-11-13 08:08:40 +0000

    [PREVIEW] Update alert query (#229)
    * Update query
    
    * Stick dummy alert email for PRs
    
    * Make sure secrets are loaded only once
    
    * Copy secret setup to parameterized too
    
    * Try out with resource as other test configs
    
    * Remove entry from output as it is only for testing

--- infrastructure/is_down_alert.tf
+++ infrastructure/is_down_alert.tf
@@ -9,30 +9,36 @@ module "send-letter-up-alert" {
   app_insights_query = <<EOF
 union
   (dependencies
+    | where timestamp > ago(3h)
     | where data == 'FtpFileUploaded' and success =~ 'true'
-    | summarize ['name'] = 'uploaded', ['counter'] = sum(itemCount)
+    | summarize ['name'] = 'uploaded', ['counter'] = sum(itemCount) by ['timecut'] = bin(timestamp, 30m)
   ),
   (requests
+    | where timestamp > ago(3h)
     | where name == 'POST SendLetterController/sendLetter' and success =~ 'true'
     | where tostring(operation_SyntheticSource) == ''
-    | summarize ['name'] = 'received', ['counter'] = sum(itemCount)
+    | summarize ['name'] = 'received', ['counter'] = sum(itemCount) by ['timecut'] = bin(timestamp, 30m)
   ),
   (traces
+    | where timestamp > ago(3h)
     | extend logger = customDimensions.LoggerName
     | where logger == 'uk.gov.hmcts.reform.sendletter.tasks.UploadLettersTask' and message has 'FTP downtime'
-    | summarize ['name'] = 'downtime', ['counter'] = sum(itemCount)
+    | summarize ['name'] = 'downtime', ['counter'] = sum(itemCount) by ['timecut'] = bin(timestamp, 30m)
   )
-| evaluate pivot(name, sum(counter))
-| project ['is_operational'] = iif(received > 0 and downtime > 0, true, iif(received > 0, uploaded > 0, true))
+| summarize ['received'] = sum(iif(name == 'received', counter, 0)),
+            ['uploaded'] = sum(iif(name == 'uploaded', counter, 0)),
+            ['downtime'] = sum(iif(name == 'downtime', counter, 0)) by bin(timecut, 30m)
+| project timecut, ['is_operational'] = iif(received > 0 and downtime > 0, true, iif(received > 0, uploaded > 0, true))
 | where is_operational == false
 EOF
 
   frequency_in_minutes       = 10
+  // window no longer matters as it is defined in the query. but it is a requirement for module
   time_window_in_minutes     = 30
   severity_level             = "2"
   action_group_name          = "${module.is-down-action-group.action_group_name}"
   custom_email_subject       = "Send Letter is DOWN"
   trigger_threshold_operator = "GreaterThan"
-  trigger_threshold          = 0
+  trigger_threshold          = 3
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
 }
--- infrastructure/is_down_alert_action_group.tf
+++ infrastructure/is_down_alert_action_group.tf
@@ -1,6 +1,12 @@
-data "azurerm_key_vault_secret" "sl_email_secret" {
+data "azurerm_key_vault_secret" "source_sl_email_secret" {
   name      = "send-letter-failure-email"
   vault_uri = "${local.permanent_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "sl_email_secret" {
+  name      = "send-letter-failure-email"
+  value     = "${data.azurerm_key_vault_secret.source_sl_email_secret.value}"
+  vault_uri = "${module.send-letter-key-vault.key_vault_uri}"
 }
 
 module "is-down-action-group" {
@@ -12,7 +18,7 @@ module "is-down-action-group" {
   action_group_name      = "Send Letter is DOWN Alert - ${var.env}"
   short_name             = "SL_is_down"
   email_receiver_name    = "Send Letter Alerts"
-  email_receiver_address = "${data.azurerm_key_vault_secret.sl_email_secret.value}"
+  email_receiver_address = "${data.azurerm_key_vault_secret.source_sl_email_secret.value}"
 }
 
 output "action_group_name" {
