4877b353a068755086662a560cf3d554eb051de0
aliveni <aliveni.ch@gmail.com>
2019-05-28 14:31:27 +0100

    Bps 609 stale letter alert (#422)
    * Stale letter alert
    
    * removed comment

---
+++ infrastructure/stale-letter-alert.tf
@@ -0,0 +1,31 @@
+module "send-letter-service-stale-letter-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Send_Letter_Service_Stale_Letter_-_BSP"
+  alert_desc = "Triggers when send letter service records a stale letter message within a 15 minutes window timeframe."
+
+  app_insights_query = <<EOF
+customEvents
+| where name == "LetterNotPrinted"
+| extend dimensions = parse_json(customDimensions)
+| project timestamp,
+          letterId = dimensions.letterId,
+          checksum = dimensions.checksum,
+          service = dimensions.service,
+          type = dimensions.type,
+          sentToPrintDayOfWeek = dimensions.sentToPrintDayOfWeek,
+          sentToPrintAt = dimensions.sentToPrintAt
+EOF
+
+  frequency_in_minutes       = 15
+  time_window_in_minutes     = 15
+  severity_level             = "2"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Send Letter Service stale letter"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold          = 0
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
