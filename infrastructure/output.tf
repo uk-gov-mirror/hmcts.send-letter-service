3e2458f1059e83496ccb464dc54d098cf99ab3e3
donatas M <1420191+doncem@users.noreply.github.com>
2019-08-21 12:16:24 +0100

    Fix pipelines: PR, master (#518)
    * Remove vault outputs
    
    * Remove `test-*` prefix
    
    * Update aliases for chart

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,3 @@
-output "vaultUri" {
-  value = "${module.send-letter-key-vault.key_vault_uri}"
-}
-
-output "vaultName" {
-  value = "${module.send-letter-key-vault.key_vault_name}"
-}
-
 output "microserviceName" {
   value = "${var.component}"
 }
