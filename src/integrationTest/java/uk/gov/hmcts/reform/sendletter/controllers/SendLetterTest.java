b5f3f77480234c15974a2da77d9916234de59b45
SomeshRA <69344273+SomeshRA@users.noreply.github.com>
2020-08-21 15:17:03 +0100

    Revert "DTSBP-18: deprecate the api which accepts templates and data (#795)" (#802)
    This reverts commit 895c9b73
    
    Co-authored-by: Somesh Acharya <somesh.acharya@hmcts.net>

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/sendletter/controllers/SendLetterTest.java
@@ -0,0 +1,84 @@
+package uk.gov.hmcts.reform.sendletter.controllers;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockFilterConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.reform.sendletter.logging.AppInsights;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "...", lazyInit = true)
+@ContextConfiguration
+@SpringBootTest
+@Transactional
+class SendLetterTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() {
+        WebRequestTrackingFilter filter = new WebRequestTrackingFilter();
+        filter.init(new MockFilterConfig());
+        mvc = webAppContextSetup(wac).addFilters(filter).build();
+    }
+
+    @SpyBean
+    private AppInsights insights;
+
+    @Test
+    void should_return_200_when_single_letter_is_sent() throws Throwable {
+        MvcResult result = send(readResource("letter.json"))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).isNotNull();
+    }
+
+    @Test
+    void should_return_400_when_bad_letter_is_sent() throws Exception {
+        send("").andExpect(status().isBadRequest());
+
+        verifyNoMoreInteractions(insights);
+    }
+
+    private ResultActions send(String content) throws Exception {
+        MockHttpServletRequestBuilder request =
+            post("/letters")
+                .header("ServiceAuthorization", "auth-header-value")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+
+        return mvc.perform(request);
+    }
+
+    private String readResource(final String fileName) throws IOException {
+        return Resources.toString(Resources.getResource(fileName), Charsets.UTF_8);
+    }
+}
--- src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/CreateLetterSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/CreateLetterSmokeTest.java
@@ -2,38 +2,24 @@ package uk.gov.hmcts.reform.sendletter.controllers;
 
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.logging.appinsights.SyntheticHeaders;
 
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
 @ExtendWith(SpringExtension.class)
 class CreateLetterSmokeTest extends SmokeTestSuite {
 
-    static Stream<Arguments> testData() {
-        return Stream.of(
-                arguments(MediaTypes.LETTER_V2, "letter-with-pdf.json"),
-                arguments(MediaTypes.LETTER_V3, "letter-with-document-count.json")
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("testData")
-    void shouldCreatLetterSuccessfully(String mediaType, String fileName) throws Exception {
+    @Test
+    void should_create_letter_successfully() throws Exception {
 
         String jwt = signIn();
 
-        String id = givenJwt(jwt, mediaType)
+        String id = givenJwt(jwt)
             .and()
-            .body(sampleLetterJson(fileName))
+            .body(sampleLetterJson("letter.json"))
             .when()
             .post("/letters")
             .then()
@@ -43,19 +29,19 @@ class CreateLetterSmokeTest extends SmokeTestSuite {
             .jsonPath()
             .get("letter_id");
 
-        givenJwt(jwt, MediaType.APPLICATION_JSON_VALUE)
+        givenJwt(jwt)
             .when()
             .get("/letters/" + id)
             .then()
             .statusCode(200);
     }
 
-    private RequestSpecification givenJwt(String jwt, String mediaType) {
+    private RequestSpecification givenJwt(String jwt) {
         return RestAssured
             .given()
             .relaxedHTTPSValidation()
             .baseUri(this.testUrl)
-            .header(HttpHeaders.CONTENT_TYPE, mediaType)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .header(SyntheticHeaders.SYNTHETIC_TEST_SOURCE, SYNTHETIC_SOURCE_HEADER_VALUE)
             .header("ServiceAuthorization", "Bearer " + jwt);
     }
