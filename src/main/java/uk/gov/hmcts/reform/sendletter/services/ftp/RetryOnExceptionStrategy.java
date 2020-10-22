023c5a59304fa2a47a0930573eb33c3ded099415
Rishi Sharma <rishi.sharma@hmcts.net>
2020-10-22 13:37:29 +0100

    DTSBPS-4 Throwing errors after maximum retries expire

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/ftp/RetryOnExceptionStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/ftp/RetryOnExceptionStrategy.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class RetryOnExceptionStrategy {
 
-    public final static int DEFAULT_RETRIES = 2;
-    public final static long DEFAULT_WAIT_TIME_IN_MILLI = 1000;
+    public static final int DEFAULT_RETRIES = 2;
+    public static final long DEFAULT_WAIT_TIME_IN_MILLI = 1000;
 
     @Value("${file-upoad.retries}")
     public void setNumberOfTriesLeft(int numberOfTriesLeft) {
