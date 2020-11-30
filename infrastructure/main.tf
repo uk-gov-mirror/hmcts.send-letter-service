1e7b5f6c4934fbb919267bd6cb31ef59edf91924
Jason Paige <jasonpaige@users.noreply.github.com>
2020-11-30 14:31:49 +0000

    Reference module as list as using a count now (#918)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -174,34 +174,34 @@ resource "azurerm_key_vault_secret" "staging_db_user" {
   count        = var.num_staging_dbs
   key_vault_id = module.send-letter-key-vault.key_vault_id
   name         = "${var.component}-staging-db-user"
-  value        = try(module.staging-db.user_name, "null")
+  value        = try(module.staging-db[0].user_name, "null")
 }
 
 resource "azurerm_key_vault_secret" "staging_db_password" {
   count        = var.num_staging_dbs
   key_vault_id = module.send-letter-key-vault.key_vault_id
   name         = "${var.component}-staging-db-password"
-  value        = try(module.staging-db.postgresql_password, "null")
+  value        = try(module.staging-db[0].postgresql_password, "null")
 }
 
 resource "azurerm_key_vault_secret" "staging_db_host" {
   count        = var.num_staging_dbs
   key_vault_id = module.send-letter-key-vault.key_vault_id
   name         = "${var.component}-staging-db-host"
-  value        = try(module.staging-db.host_name, "null")
+  value        = try(module.staging-db[0].host_name, "null")
 }
 
 resource "azurerm_key_vault_secret" "staging_db_port" {
   count        = var.num_staging_dbs
   key_vault_id = module.send-letter-key-vault.key_vault_id
   name         = "${var.component}-staging-db-port"
-  value        = try(module.staging-db.postgresql_listen_port, "null")
+  value        = try(module.staging-db[0].postgresql_listen_port, "null")
 }
 
 resource "azurerm_key_vault_secret" "staging_db_name" {
   count        = var.num_staging_dbs
   key_vault_id = module.send-letter-key-vault.key_vault_id
   name         = "${var.component}-staging-db-name"
-  value        = try(module.staging-db.postgresql_database, "null")
+  value        = try(module.staging-db[0].postgresql_database, "null")
 }
 # endregion
