b2301f0e8bc4d84e96a0903401a3d853614b93eb
Jason Paige <jasonpaige@users.noreply.github.com>
2020-12-01 09:25:25 +0000

    Remove unused pg9.6 RPE dbs (#915)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,40 +9,12 @@ resource "azurerm_resource_group" "rg" {
 }
 
 locals {
-  ase_name = "core-compute-${var.env}"
-
   ftp_private_key = data.azurerm_key_vault_secret.ftp_private_key.value
   ftp_public_key  = data.azurerm_key_vault_secret.ftp_public_key.value
   ftp_user        = data.azurerm_key_vault_secret.ftp_user.value
 
   encryption_public_key = data.azurerm_key_vault_secret.encryption_public_key.value
 
-  local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "aat" : "saat" : var.env
-  local_ase = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "core-compute-aat" : "core-compute-saat" : local.ase_name
-
-  s2s_rg  = "rpe-service-auth-provider-${local.local_env}"
-  s2s_url = "http://${local.s2s_rg}.service.core-compute-${local.local_env}.internal"
-
-  previewVaultName    = "${var.product}-send-letter"
-  nonPreviewVaultName = "${var.product}-send-letter-${var.env}"
-  vaultName           = (var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName
-
-  db_connection_options = "?sslmode=require"
-
-  sku_size = var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"
-}
-
-module "db" {
-  source          = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product         = "${var.product}-${var.component}-db"
-  location        = var.location_db
-  env             = var.env
-  database_name   = "send_letter"
-  postgresql_user = "send_letter"
-  sku_name        = "GP_Gen5_2"
-  sku_tier        = "GeneralPurpose"
-  common_tags     = var.common_tags
-  subscription    = var.subscription
 }
 
 module "db-v11" {
@@ -77,7 +49,7 @@ module "staging-db" {
 # region save DB details to Azure Key Vault
 module "send-letter-key-vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
-  name                = local.vaultName
+  name                = "${var.product}-send-letter-${var.env}"
   product             = var.product
   env                 = var.env
   tenant_id           = var.tenant_id
@@ -92,8 +64,8 @@ module "send-letter-key-vault" {
 }
 
 data "azurerm_key_vault" "s2s_key_vault" {
-  name                = "s2s-${local.local_env}"
-  resource_group_name = local.s2s_rg
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,7 +1,7 @@
 #region Data for tests
 
 output "test_s2s_url" {
-  value = "${local.s2s_url}"
+  value = "http://rpe-service-auth-provider-${var.env}.service.core-compute-${var.env}.internal"
 }
 
 output "test_s2s_name" {
@@ -9,27 +9,27 @@ output "test_s2s_name" {
 }
 
 output "test_ftp_hostname" {
-  value = "${var.ftp_hostname}"
+  value = var.ftp_hostname
 }
 
 output "test_ftp_port" {
-  value = "${var.ftp_port}"
+  value = var.ftp_port
 }
 
 output "test_ftp_fingerprint" {
-  value = "${var.ftp_fingerprint}"
+  value = var.ftp_fingerprint
 }
 
 output "test_ftp_target_folder" {
-  value = "${var.ftp_smoke_test_target_folder}"
+  value = var.ftp_smoke_test_target_folder
 }
 
 output "ftp_target_folder" {
-  value = "${var.ftp_target_folder}"
+  value = var.ftp_target_folder
 }
 
 output "test_encryption_enabled" {
-  value = "${var.encyption_enabled}"
+  value = var.encyption_enabled
 }
 
 #endregion
