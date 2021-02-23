783a811158eb8f7dd13ea5c74c6c5929dd69c3f1
gauravchawla <gaurav-chawla@hotmail.com>
2021-02-23 23:23:08 +0000

    A flag created to disable downtime.

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/ftp/FtpAvailabilityChecker.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/ftp/FtpAvailabilityChecker.java
@@ -6,13 +6,18 @@ public class FtpAvailabilityChecker implements IFtpAvailabilityChecker {
 
     private final LocalTime downtimeStart;
     private final LocalTime downtimeEnd;
+    private final boolean disableDowntime;
 
-    public FtpAvailabilityChecker(String downtimeFromHour, String downtimeToHour) {
+    public FtpAvailabilityChecker(String downtimeFromHour, String downtimeToHour, Boolean disableDowntime) {
         this.downtimeStart = LocalTime.parse(downtimeFromHour);
         this.downtimeEnd = LocalTime.parse(downtimeToHour);
-    }
+        this.disableDowntime=disableDowntime;
 
+    }
     public boolean isFtpAvailable(LocalTime time) {
+        if (disableDowntime){
+            return true;
+        }
         if (downtimeStart.isBefore(downtimeEnd)) {
             return time.isBefore(downtimeStart) || time.isAfter(downtimeEnd);
         } else {
