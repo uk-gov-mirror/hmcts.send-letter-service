a776b76a955518fa296e14fa80f96ed32a5e0bb5
SomeshRA <somesh.acharya1@hmcts.net>
2021-04-06 18:16:16 +0100

    DTSBPS-427: Controller for generating SAS token and unique request id

--- src/main/java/uk/gov/hmcts/reform/sendletter/controllers/SasTokenController.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/controllers/SasTokenController.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sendletter.controllers;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.sendletter.model.out.SasTokenResponse;
+import uk.gov.hmcts.reform.sendletter.model.out.SendLetterResponse;
 
 import java.util.UUID;
 
@@ -17,8 +19,12 @@ import java.util.UUID;
 public class SasTokenController {
 
     @GetMapping(path = "/{serviceName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Get SAS Token to access blob storage")
-    @ApiResponse(code = 200, message = "Success")
+    @ApiOperation(value = "Get SAS Token to access blob storage and unique request id")
+    @ApiResponses({
+        @ApiResponse(code = 200, response = SendLetterResponse.class, message = "Successfully sent letter"),
+        @ApiResponse(code = 401, message = ControllerResponseMessage.RESPONSE_401),
+        @ApiResponse(code = 403, message = ControllerResponseMessage.RESPONSE_403)
+    })
     public ResponseEntity<SasTokenResponse> getSasToken(@PathVariable String serviceName) {
         //TODO replace with actual calls
         String sasToken = "dummy";
