4c821cd270e47e968dde699dba9c75beac3ecc6d
SomeshRA <somesh.acharya1@hmcts.net>
2021-04-07 17:20:55 +0100

    DTSBPS-427: Controller for generating SAS token and unique request id

--- src/main/java/uk/gov/hmcts/reform/sendletter/services/SasTokenGeneratorService.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/services/SasTokenGeneratorService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class SasTokenGeneratorService {
 
     public String generateSasToken(String serviceName) {
-        return "dummy";
+        return serviceName;
     }
 }
